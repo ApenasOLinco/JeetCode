43900ace065e60604763d5dde8990c35ab4db18c
apenasolinco <120327456+ApenasOLinco@users.noreply.github.com>
2026-01-17 14:32:56 -0300

    remove unused import

--- src/com/apenasolinco/leetcodae/FindFirstIndexOfStringOccurence28.java
+++ src/com/apenasolinco/leetcodae/FindFirstIndexOfStringOccurence28.java
@@ -1,7 +1,5 @@
 package com.apenasolinco.leetcodae;
 
-import java.text.Collator;
-
 /**
  * <a href="https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/">
  * Leet 28: Find the Index of the First Occurrence in a String
