f691d5bbf81d6fed9d894220953d8fde67489c85
apenasolinco <120327456+ApenasOLinco@users.noreply.github.com>
2026-01-23 17:43:06 -0300

    Leet 58 - Length of Last Word

---
+++ src/com/apenasolinco/leetcodae/LengthOfLastWord58.java
@@ -0,0 +1,42 @@
+package com.apenasolinco.leetcodae;
+
+public class LengthOfLastWord58 {
+    public static void main(String[] args) {
+        LengthOfLastWord58 l58 = new LengthOfLastWord58();
+
+        // Expected result: 5
+        System.out.println(l58.lengthOfLastWord("Hello World"));
+
+        // Expected result: 4
+        System.out.println(l58.lengthOfLastWord("   fly me   to   the moon  "));
+
+        // Expected result: 6
+        System.out.println(l58.lengthOfLastWord("luffy is still joyboy"));
+
+        // Expected result: 1
+        System.out.println(l58.lengthOfLastWord("a"));
+
+        // Expected result: 3
+        System.out.println(l58.lengthOfLastWord("day"));
+    }
+
+    public int lengthOfLastWord(String s) {
+        int l = -1, r = s.length() - 1;
+
+        while (r >= 0) {
+            if (s.charAt(r) == ' ') {
+                r--;
+                continue;
+            }
+
+            l = r - 1;
+            while (l >= 0 && s.charAt(l) != ' ') {
+                l--;
+            }
+
+            break;
+        }
+
+        return r - l;
+    }
+}
